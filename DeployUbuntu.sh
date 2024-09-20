a07963b4a8ddf2668bae151c43bebe66fa72603c
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2024-09-20 09:10:34 -0400

    Update DeployUbuntu.sh

--- DeployUbuntu.sh
+++ DeployUbuntu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# version 1.10.06
+# version 1.10.07
 # make TAB's folder
 mkdir /etc/tab
 # create LT bounce script
-echo “pkill -9 ltechagent; sudo /etc/init.d/ltechagent start” > /bin/bouncelt.sh
+echo "pkill -9 ltechagent; sudo /etc/init.d/ltechagent start" > /bin/bouncelt.sh
 chmod +xX /bin/bouncelt.sh
 # create screenconnect bounce script
-echo “/etc/init.d/connectwisecontrol-24a22b9fc261d141 stop; /etc/init.d/connectwisecontrol-24a22b9fc261d141 start” > /bin/bouncescreencon.sh
+echo "/etc/init.d/connectwisecontrol-24a22b9fc261d141 stop; /etc/init.d/connectwisecontrol-24a22b9fc261d141 start" > /bin/bouncescreencon.sh
 chmod +xX /bin/bouncescreencon.sh
 # install webmin
 rm -f /usr/share/keyrings/webmin.gpg
