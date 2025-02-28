e0b85bca76578db6909b652093fb4b45776afa58
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-02-28 15:50:03 -0500

    Update bouncesc.sh

--- bouncesc.sh
+++ bouncesc.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/etc/init.d/connectwisecontrol-24a22b9fc261d141 stop
-/etc/init.d/connectwisecontrol-24a22b9fc261d141 start
+echo "Stopping service ..."
+service connectwisecontrol-24a22b9fc261d141 stop 2> /dev/null
+service connectwisecontrol-24a22b9fc261d141 status
+echo "Starting service ..."
+service connectwisecontrol-24a22b9fc261d141 start 2> /dev/null
+service connectwisecontrol-24a22b9fc261d141 status
