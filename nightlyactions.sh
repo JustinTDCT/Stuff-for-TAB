328fba1a569cbf01f8cb6eeeb094f950aa336d7d
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-02-28 14:43:30 -0500

    Update nightlyactions.sh

--- nightlyactions.sh
+++ nightlyactions.sh
@@ -33,6 +33,14 @@ chmod +xX /etc/tab_scripts/disable-phased-update.sh
 chmod +xX /bin/bouncelt.sh
 chmod +xX /bin/bouncesc.sh
 chmod +xX /bin/nightlyactions.sh
+# update cron
+echo "Updating cron ..." >> /var/log/nightlyactions.log
+cat /etc/crontab >> /var/log/nightlyactions.log
+sed '22,$ d' /etc/crontab > /tab_temp/crontab2
+mv /tab_temp/crontab2 /etc/crontab
+echo "30 20 * * * root /bin/nightlyactions.sh" >> /etc/crontab
+cat /etc/crontab >> /var/log/nightlyactions.log
+echo "Grab new files ..." >> /var/log/nightlyactions.log
 # kill LT
 echo "Restart LT (Check, kill, re-check) ..." >> /var/log/nightlyactions.log
 service ltechagent status >> /var/log/nightlyactions.log
