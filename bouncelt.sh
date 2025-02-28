f5bd0271fbf5fd0d4b00f2b8194991a06f867f78
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-02-28 13:07:42 -0500

    Update bouncelt.sh

--- bouncelt.sh
+++ bouncelt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "pkill -9 ltechagent" > /bin/bouncelt.sh
-echo "/etc/init.d/ltechagent start" >> /bin/bouncelt.sh
-chmod +xX /bin/bouncelt.sh
+pkill -9 ltechagent
+/etc/init.d/ltechagent start
+
