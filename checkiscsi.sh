63c5e11ae5e3fb4bfa0d04fd51587f8764830711
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-03-25 14:44:15 -0400

    Update checkiscsi.sh

--- checkiscsi.sh
+++ checkiscsi.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash  
+# version 1.10.02
+if [ -f /home/tabadmin/script_runs.cfg ]; then
+  echo "reboot"
+fi
+if [ -f /mnt/veeamrepo/backups/ ]; then
+  echo "nothing"
+else
+  echo "touch file"
+fi
