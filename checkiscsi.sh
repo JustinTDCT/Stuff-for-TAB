95b54e35abfe76c4ae47d112a70e452bd79a011a
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-03-31 09:26:40 -0400

    Update checkiscsi.sh

--- checkiscsi.sh
+++ checkiscsi.sh
@@ -9,6 +9,8 @@ if [ -d /mnt/veeamrepo/backups/ ]; then
   echo "iSCSI OK"
   if [ -f /etc/tab_scripts/iscsi.fail ]; then
     echo "Removing old fail file..."
+    echo "$(date)" >> /var/log/nightlyactions.log
+    echo "- iSCSI reconnected" > /var/log/nightlyactions.log
     rm -f /etc/tab_scripts/iscsi.fail
   fi
 else
@@ -16,9 +18,13 @@ else
     echo "2nd fail, rebooting..."
     echo "Removing old fail file..."
     rm -f /etc/tab_scripts/iscsi.fail
+    echo "$(date)" >> /var/log/nightlyactions.log
+    echo "- 2nd fail, rebooting" > /var/log/nightlyactions.log
     shutdown -r now
   else  
     echo "iSCSI fail - dropping file..."
     touch /etc/tab_scripts/iscsi.fail
+    echo "$(date)" >> /var/log/nightlyactions.log
+    echo "- 1st fail, dropping file" > /var/log/nightlyactions.log
   fi
 fi
